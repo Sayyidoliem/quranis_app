f081efe58bdcf4cc0bb08fd1745d164bd9ba66c5
Sayyid Olim <sayyid.olim12@gmail.com>
2023-10-16 10:31:27 +0700

    Nambahin compass solat

---
+++ app/src/main/java/com/example/quranisapp/FindQiblat.kt
@@ -0,0 +1,124 @@
+package com.example.quranisapp
+
+import android.app.Activity
+import android.content.Intent
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.viewinterop.AndroidView
+import com.just.agentweb.AgentWeb
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FindQiblat(
+    back: () -> Unit
+) {
+    val activity = LocalContext.current as Activity
+    val context = LocalContext.current
+    val url = "https://qiblafinder.withgoogle.com/intl/id/"
+    var expanded by remember { mutableStateOf(false) }
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Qiblat", color = MaterialTheme.colorScheme.onPrimary) },
+                navigationIcon = {
+                    IconButton(onClick = { back() }) {
+                        Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null, tint = MaterialTheme.colorScheme.onPrimary)
+                    }
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = MaterialTheme.colorScheme.primary),
+                actions = {
+                    IconButton(onClick = { expanded = true }) {
+                        Icon(
+                            imageVector = Icons.Default.MoreVert,
+                            contentDescription = "",
+                            tint = MaterialTheme.colorScheme.onPrimary
+                        )
+                    }
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        DropdownMenuItem(
+                            text = { Text("Share") },
+                            onClick = {
+                                val sendIntent: Intent = Intent().apply {
+                                    action = Intent.ACTION_SEND
+                                    putExtra(Intent.EXTRA_TEXT, "Qiblat Finder by : $url" )
+                                    type = "text/plain"
+                                }
+                                val shareIntent = Intent.createChooser(sendIntent, null)
+                                context.startActivity(shareIntent)
+                                expanded = false
+                            },
+                            leadingIcon = {
+                                Icon(
+                                    Icons.Outlined.Share,
+                                    contentDescription = null
+                                )
+                            })
+                        DropdownMenuItem(
+                            text = { Text("Send Feedback") },
+                            onClick = { expanded = false },
+                            leadingIcon = {
+                                Icon(
+                                    Icons.Outlined.Email,
+                                    contentDescription = null
+                                )
+                            },
+                            trailingIcon = { Text("F11", textAlign = TextAlign.Center) })
+                    }
+                }
+            )
+        },
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+            contentAlignment = Alignment.Center
+        ) {
+            AndroidView(//ambil view yg ada di xml buat compose
+                factory = { context ->
+                    LinearLayout(context).apply {
+                        layoutParams = ViewGroup.LayoutParams(//parameter kayak width n height
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT
+                        )
+                        AgentWeb.with(activity)
+                            .setAgentWebParent(this, this.layoutParams)
+                            .useDefaultIndicator()
+                            .createAgentWeb()
+                            .ready()
+                            .go(url)//masukin urlnya
+                    }
+                }
+            )
+        }
+    }
+}
