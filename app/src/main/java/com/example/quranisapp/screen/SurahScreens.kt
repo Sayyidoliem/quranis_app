1bb645c7713716bac5e56d64f0aeaf8c09fed97b
Sayyid Olim <sayyid.olim12@gmail.com>
2023-11-17 02:25:27 +0700

    Fix UI QuranScreens.kt

--- app/src/main/java/com/example/quranisapp/screen/SurahScreens.kt
+++ app/src/main/java/com/example/quranisapp/screen/SurahScreens.kt
@@ -1,9 +1,7 @@
-package com.example.quranisapp.tabrowscreens
+package com.example.quranisapp.screen
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,13 +10,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.BookmarkAdd
+import androidx.compose.material.icons.filled.BookmarkAdded
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,12 +31,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.quranisapp.data.database.BookmarkDatabase
 import com.example.quranisapp.data.database.QoranDatabase
 import com.example.quranisapp.data.database.entities.SurahBookmark
@@ -46,7 +43,7 @@ import kotlinx.coroutines.launch
 @Composable
 fun SurahScreens(
     modifier: Modifier = Modifier,
-    goToRead: (surahNumber: Int?, juzNumber: Int?, pageNumber: Int?) -> Unit
+    goToRead: (surahNumber: Int?, juzNumber: Int?, pageNumber: Int?, index: Int?) -> Unit
 ) {
 
     val context = LocalContext.current
@@ -54,7 +51,6 @@ fun SurahScreens(
     val list = dao.getSurah()
 
     list.collectAsState(initial = emptyList()).let { state ->
-        print(state.value.toString())
         LazyColumn(
             modifier = modifier
                 .fillMaxSize()
@@ -66,15 +62,22 @@ fun SurahScreens(
                         .clip(RoundedCornerShape(10))
 //                    .padding(horizontal = 16.dp, vertical = 8.dp)
                         .clickable {
-                            goToRead.invoke(surah.surahNumber, null, null)
+                            goToRead.invoke(surah.surahNumber, null, null, null)
                         },
                     colors = CardDefaults.cardColors(
                         containerColor = MaterialTheme.colorScheme.background
                     )
 
                 ) {
-                    Box(Modifier.fillMaxSize()) {
-                        Row {
+                    ListItem(
+                        headlineContent = {
+                            Text(
+                                text = "${surah.surahNameEn} | ${surah.surahNameAr}",
+                                fontWeight = FontWeight.Bold,
+                                style = MaterialTheme.typography.titleLarge
+                            )
+                        },
+                        leadingContent = {
                             Card(
                                 modifier = Modifier
                                     .padding(12.dp)
@@ -86,36 +89,30 @@ fun SurahScreens(
                                 Box(Modifier.fillMaxSize()) {
                                     Text(
                                         text = "${surah.surahNumber}",
+                                        style = MaterialTheme.typography.bodyLarge,
                                         modifier = Modifier.align(Alignment.Center)
                                     )
                                 }
                             }
-                            Column(modifier = Modifier.padding(8.dp)) {
-                                Text(
-                                    text = "${surah.surahNameEn} | ${surah.surahNameAr}",
-                                    fontSize = 18.sp,
-                                    fontWeight = FontWeight.Bold
-                                )
-                                Text(text = "${surah.numberOfAyah} Ayat")
-                                Text(text = "Juz ${surah.juzNumber} | ${surah.surahDescendPlace}")
-                            }
-                            Box(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(16.dp), Alignment.TopEnd
-                            ) {
-                                SurahFavoriteButton(
-                                    modifier = Modifier.align(Alignment.Center),
-                                    totalAyah = surah.numberOfAyah,
-                                    surahNumber = surah.surahNumber,
-                                    juzNumber = surah.juzNumber,
-                                    surahNameEn = surah.surahNameEn,
-                                    surahNameAr = surah.surahNameAr,
-                                    surahDescend = surah.surahDescendPlace
-                                )
-                            }
+                        },
+                        supportingContent = {
+                            Text(
+                                text = "${surah.numberOfAyah} Ayat | Juz ${surah.juzNumber} | ${surah.surahDescendPlace}",
+                                style = MaterialTheme.typography.titleMedium
+                            )
+                        },
+                        trailingContent = {
+                            SurahFavoriteButton(
+                                modifier = Modifier,
+                                totalAyah = surah.numberOfAyah,
+                                surahNumber = surah.surahNumber,
+                                juzNumber = surah.juzNumber,
+                                surahNameEn = surah.surahNameEn,
+                                surahNameAr = surah.surahNameAr,
+                                surahDescend = surah.surahDescendPlace,
+                            )
                         }
-                    }
+                    )
                     Divider(modifier = Modifier.padding(horizontal = 16.dp), thickness = 2.dp)
                 }
             }
@@ -126,17 +123,15 @@ fun SurahScreens(
 @Composable
 fun SurahFavoriteButton(
     modifier: Modifier = Modifier,
-    color: Color = Color(0xffE91E63),
     surahNumber: Int?,
     surahNameEn: String?,
     surahNameAr: String?,
     totalAyah: Int?,
     juzNumber: Int?,
     surahDescend: String?,
-    surahBookmark: SurahBookmark? = null,
 ) {
     val context = LocalContext.current
-    val dao = BookmarkDatabase.getInstance(context).dao()
+    val dao = BookmarkDatabase.getInstance(context).bookmarkDao()
     val scope = rememberCoroutineScope()
     var isFavorite by remember { mutableStateOf(false) }
 
@@ -148,34 +143,43 @@ fun SurahFavoriteButton(
         checked = isFavorite,
         onCheckedChange = {
             scope.launch {
-                if (isFavorite && surahBookmark != null) {
-                    dao.deleteSurahBookmark(surahBookmark)
-                    isFavorite = false
-                } else {
-                    dao.insertSurahBookmark(
+                if (isFavorite) {
+                    dao.deleteSurahBookmark(
                         SurahBookmark(
                             surahNameEn!!,
                             surahNameAr!!,
                             totalAyah!!,
                             juzNumber!!,
-                            surahDescend!!
+                            surahDescend,
+                            surahNumber
                         )
                     )
-                    isFavorite = true
+                    isFavorite = false
+                    return@launch
                 }
+                dao.insertSurahBookmark(
+                    SurahBookmark(
+                        surahNameEn!!,
+                        surahNameAr!!,
+                        totalAyah!!,
+                        juzNumber!!,
+                        surahDescend!!,
+                        surahNumber
+                    )
+                )
+                isFavorite = true
             }
         }
     ) {
         Icon(
-            tint = color,
             modifier = modifier.graphicsLayer {
                 scaleX = 1.3f
                 scaleY = 1.3f
             },
             imageVector = if (isFavorite) {
-                Icons.Filled.Favorite
+                Icons.Filled.BookmarkAdded
             } else {
-                Icons.Default.FavoriteBorder
+                Icons.Default.BookmarkAdd
             },
             contentDescription = null
         )
